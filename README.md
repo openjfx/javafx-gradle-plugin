20509b9bea3694f61083118e669a716a0de89fc4
Joeri Sykora <joeri.sykora@gluonhq.com>
2018-11-20 12:54:02 +0100

    no longer auto-detect the javafx modules by inspecting module-info.java

--- README.md
+++ README.md
@@ -4,24 +4,35 @@ Simplifies working with JavaFX 11+ for gradle projects.
 
 ## Getting started
 
-### Modular projects
+### Apply the plugin
 
-For modular projects, you only need to apply the plugin. The plugin will automatically add the required JavaFX dependencies based on the module definition.
-
-    plugins {
-        id 'java'
-        id 'org.openjfx.javafxplugin' version '0.0.3'
-    }
-
-### Non-modular projects
-
-For non-modular projects, you need to apply the plugin and define the list of JavaFX modules required by the project.
+Using the `plugins` DSL:
 
     plugins {
         id 'java'
         id 'org.openjfx.javafxplugin' version '0.0.3'
     }
-    
+
+Alternatively, you can use the `buildscript` DSL:
+
+    buildscript {
+        repositories {
+            maven {
+                url "https://plugins.gradle.org/m2/"
+            }
+        }
+        dependencies {
+            classpath 'org.openjfx:javafx-plugin:0.0.3'
+        }
+    }
+
+    apply plugin: 'java'
+    apply plugin: 'org.openjfx.javafxplugin'
+
+### Specify JavaFX modules
+
+Specify all the JavaFX modules that your project uses:
+
     javafx {
-        modules = [ 'javafx.controls' ]
+        modules = [ 'javafx.controls', 'javafx.fxml' ]
     }
--- src/main/java/org/openjfx/gradle/JavaFXPlugin.java
+++ src/main/java/org/openjfx/gradle/JavaFXPlugin.java
@@ -1,29 +1,16 @@
 package org.openjfx.gradle;
 
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ParserConfiguration;
-import com.github.javaparser.ast.modules.ModuleDeclaration;
-import com.github.javaparser.ast.modules.ModuleStmt;
-import com.github.javaparser.ast.nodeTypes.NodeWithName;
 import com.google.gradle.osdetector.OsDetector;
 import com.google.gradle.osdetector.OsDetectorPlugin;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.UnknownDomainObjectException;
 import org.gradle.api.plugins.ApplicationPlugin;
-import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.JavaExec;
-import org.gradle.api.tasks.SourceSet;
 import org.javamodularity.moduleplugin.ModuleSystemPlugin;
 import org.javamodularity.moduleplugin.tasks.ModuleOptions;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -66,68 +53,27 @@ public class JavaFXPlugin implements Plugin<Project> {
 
     private void applyDependencies(Project project) {
         project.afterEvaluate(c -> {
-            var javafxModules = new TreeSet<>(project.getExtensions().getByType(JavaFXOptions.class).getModules());
-            String moduleName = (String) project.getExtensions().findByName("moduleName");
-            if (moduleName != null) {
-                detectModule(project).ifPresent(moduleDeclaration1 ->
-                        javafxModules.addAll(moduleDeclaration1.getModuleStmts().stream()
-                                .filter(ModuleStmt::isModuleRequiresStmt)
-                                .map(ModuleStmt::asModuleRequiresStmt)
-                                .map(NodeWithName::getNameAsString)
-                                .filter(JAVAFX_DEPENDENCIES::containsKey)
-                                .collect(Collectors.toList())));
-            }
-
-            Set<String> uniqueJavaFXModules = javafxModules.stream()
-                    .flatMap(javafxModule -> Stream.concat(Stream.of(javafxModule), JAVAFX_DEPENDENCIES.get(javafxModule).stream()))
-                    .collect(Collectors.toSet());
+            var definedJavaFXModules = new TreeSet<>(project.getExtensions().getByType(JavaFXOptions.class).getModules());
 
             JavaExec execTask = (JavaExec) project.getTasks().findByName(ApplicationPlugin.TASK_RUN_NAME);
             if (execTask != null) {
                 ModuleOptions moduleOptions = execTask.getExtensions().findByType(ModuleOptions.class);
                 if (moduleOptions != null) {
-                    uniqueJavaFXModules.forEach(m -> {
-                        moduleOptions.getAddModules().add(m);
-                    });
+                    definedJavaFXModules.forEach(javaFXModule -> moduleOptions.getAddModules().add(javaFXModule));
                 }
             }
 
-            String version = project.getExtensions().getByType(JavaFXOptions.class).getVersion();
-            uniqueJavaFXModules.stream()
-                    .map(module -> module.replace(".", "-"))
-                    .forEach(artifact -> {
+            var allJavaFXModules = definedJavaFXModules.stream()
+                    .flatMap(javaFXModule -> Stream.concat(Stream.of(javaFXModule), JAVAFX_DEPENDENCIES.get(javaFXModule).stream()))
+                    .collect(Collectors.toSet());
+
+            String javaFXVersion = project.getExtensions().getByType(JavaFXOptions.class).getVersion();
+            allJavaFXModules.stream()
+                    .map(javaFXModule -> javaFXModule.replace(".", "-"))
+                    .forEach(javaFXArtifact -> {
                 project.getDependencies().add("compile",
-                        String.format("org.openjfx:%s:%s:%s", artifact, version, platform));
+                        String.format("org.openjfx:%s:%s:%s", javaFXArtifact, javaFXVersion, platform));
             });
         });
-    }
-
-    private Optional<ModuleDeclaration> detectModule(Project project) {
-        SourceSet main;
-        try {
-            JavaPluginConvention javaConvention = project.getConvention().getPlugin(JavaPluginConvention.class);
-            main = javaConvention.getSourceSets().getByName(SourceSet.MAIN_SOURCE_SET_NAME);
-        } catch (IllegalStateException | UnknownDomainObjectException e) {
-            return Optional.empty();
-        }
-
-        Optional<File> moduleInfoSrcDir = main.getAllJava()
-                .getSourceDirectories()
-                .getFiles()
-                .stream()
-                .filter(dir -> dir.toPath().resolve("module-info.java").toFile().exists())
-                .findAny();
-
-        if (moduleInfoSrcDir.isPresent()) {
-            Path moduleInfoJava = moduleInfoSrcDir.get().toPath().resolve("module-info.java");
-            JavaParser.getStaticConfiguration().setLanguageLevel(ParserConfiguration.LanguageLevel.JAVA_11);
-            try {
-                return JavaParser.parse(moduleInfoJava).getModule();
-            } catch (IOException e) {
-                project.getLogger().error("Error opening module-info.java in source dir {}", moduleInfoSrcDir.get());
-            }
-        }
-
-        return Optional.empty();
     }
 }
