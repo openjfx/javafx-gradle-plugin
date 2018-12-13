9043ad34c816d0d472e0ce26a6c479837d6b69a5
Joeri Sykora <joeri.sykora@gluonhq.com>
2018-12-13 09:53:14 +0100

    expose detected platform

--- src/main/java/org/openjfx/gradle/JavaFXOptions.java
+++ src/main/java/org/openjfx/gradle/JavaFXOptions.java
@@ -1,35 +1,28 @@
 package org.openjfx.gradle;
 
 import com.google.gradle.osdetector.OsDetector;
-import org.gradle.api.GradleException;
 import org.gradle.api.Project;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.Optional;
-import java.util.TreeSet;
 
 public class JavaFXOptions {
 
-    private String version = "11.0.1";
-    private final List<String> modules = new ArrayList<>();
+    private static final String MAVEN_JAVAFX_ARTIFACT_GROUP_ID = "org.openjfx";
+
     private final Project project;
-    private String platform;
+    private final String platform;
+
+    private String version = "11.0.1";
+    private List<String> modules = new ArrayList<>();
 
     public JavaFXOptions(Project project) {
-	this.project = project;
-        detectPlatform(project);
+        this.project = project;
+        this.platform = detectPlatform();
     }
 
-    private void detectPlatform(Project project) {
-        String os = project.getExtensions().getByType(OsDetector.class).getOs();
-        platform = os;
-        if ("osx".equals(os)) {
-            platform = "mac";
-        } else if ("windows".equals(os)) {
-            platform = "win";
-        }
+    public String getPlatform() {
+        return platform;
     }
 
     public String getVersion() {
@@ -38,6 +31,7 @@ public class JavaFXOptions {
 
     public void setVersion(String version) {
         this.version = version;
+        updateJavaFXDependencies();
     }
 
     public List<String> getModules() {
@@ -45,36 +39,40 @@ public class JavaFXOptions {
     }
 
     public void setModules(List<String> modules) {
-	this.modules.clear();
-        this.modules.addAll(modules);
-
-	validateModules();
-
-	var definedJavaFXModuleNames = new TreeSet<>(getModules());
-
-        var javaFXModules = definedJavaFXModuleNames.stream()
-                    .map(JavaFXModule::fromModuleName)
-                    .flatMap(Optional::stream)
-                    .flatMap(javaFXModule -> javaFXModule.getMavenDependencies().stream())
-                    .collect(Collectors.toSet());
-
-        javaFXModules.forEach(javaFXModule -> {
-                project.getDependencies().add("implementation",
-                        String.format("org.openjfx:%s:%s:%s", javaFXModule.getArtifactName(), getVersion(), platform));
-	    });
+        this.modules = modules;
+        updateJavaFXDependencies();
     }
 
     public void modules(String...moduleNames) {
         setModules(List.of(moduleNames));
     }
 
-    public void validateModules() {
-        List<String> invalidModules = this.modules.stream()
-                .filter(module -> JavaFXModule.fromModuleName(module).isEmpty())
-                .collect(Collectors.toList());
+    private String detectPlatform() {
+        String os = project.getExtensions().getByType(OsDetector.class).getOs();
+        String platform = os;
+        if ("osx".equals(os)) {
+            platform = "mac";
+        } else if ("windows".equals(os)) {
+            platform = "win";
+        }
+        return platform;
+    }
 
-        if (! invalidModules.isEmpty()) {
-            throw new GradleException("Found one or more invalid JavaFX module names: " + invalidModules);
+    private void updateJavaFXDependencies() {
+        clearJavaFXDependencies();
+
+        JavaFXModule.getJavaFXModules(this.modules).forEach(javaFXModule -> {
+            project.getDependencies().add("implementation",
+                    String.format("%s:%s:%s:%s", MAVEN_JAVAFX_ARTIFACT_GROUP_ID, javaFXModule.getArtifactName(),
+                            getVersion(), getPlatform()));
+        });
+    }
+
+    private void clearJavaFXDependencies() {
+        var implementationConfiguration = project.getConfigurations().findByName("implementation");
+        if (implementationConfiguration != null) {
+            implementationConfiguration.getDependencies()
+                    .removeIf(dependency -> MAVEN_JAVAFX_ARTIFACT_GROUP_ID.equals(dependency.getGroup()));
         }
     }
 }
--- src/test/java/org/openjfx/gradle/JavaFXModuleTest.java
+++ src/test/java/org/openjfx/gradle/JavaFXModuleTest.java
@@ -1,5 +1,6 @@
 package org.openjfx.gradle;
 
+import org.gradle.api.GradleException;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class JavaFXModuleTest {
 
@@ -40,6 +42,25 @@ class JavaFXModuleTest {
         assertTrue(dependencies.contains(module));
         for (JavaFXModule dependentModule : JavaFXModule.CONTROLS.getDependentModules()) {
             assertTrue(dependencies.contains(dependentModule));
+        }
+    }
+
+    @Test
+    void validateWithValidModules() {
+        var moduleNames = List.of(JavaFXModule.CONTROLS.getModuleName(), JavaFXModule.WEB.getModuleName());
+
+        JavaFXModule.validateModules(moduleNames);
+    }
+
+    @Test
+    void validateWithInvalidModules() {
+        var moduleNames = List.of("javafx.unknown", JavaFXModule.CONTROLS.getModuleName(), JavaFXModule.WEB.getModuleName());
+
+        try {
+            JavaFXModule.validateModules(moduleNames);
+            fail("Validate Modules must throw GradleException.");
+        } catch (GradleException e) {
+            // expected
         }
     }
 }
