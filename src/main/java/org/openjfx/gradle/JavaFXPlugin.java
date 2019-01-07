45192aaefb9c3cc95e17d1f696fc53b8737ded60
Joeri Sykora <joeri.sykora@gluonhq.com>
2019-01-07 11:55:55 +0100

    exec task for non-modular projects is configured by this plugin (#24)

--- src/main/java/org/openjfx/gradle/JavaFXPlugin.java
+++ src/main/java/org/openjfx/gradle/JavaFXPlugin.java
@@ -32,12 +32,8 @@ package org.openjfx.gradle;
 import com.google.gradle.osdetector.OsDetectorPlugin;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.plugins.ApplicationPlugin;
-import org.gradle.api.tasks.JavaExec;
 import org.javamodularity.moduleplugin.ModuleSystemPlugin;
-import org.javamodularity.moduleplugin.tasks.ModuleOptions;
-
-import java.util.TreeSet;
+import org.openjfx.gradle.tasks.ExecTask;
 
 public class JavaFXPlugin implements Plugin<Project> {
 
@@ -48,23 +44,6 @@ public class JavaFXPlugin implements Plugin<Project> {
 
         project.getExtensions().create("javafx", JavaFXOptions.class, project);
 
-        updateApplicationRunTaskModules(project);
-    }
-
-    private void updateApplicationRunTaskModules(Project project) {
-        project.afterEvaluate(c -> {
-            JavaExec execTask = (JavaExec) project.getTasks().findByName(ApplicationPlugin.TASK_RUN_NAME);
-            if (execTask != null) {
-                ModuleOptions moduleOptions = execTask.getExtensions().findByType(ModuleOptions.class);
-                if (moduleOptions != null) {
-                    JavaFXOptions javaFXOptions = project.getExtensions().getByType(JavaFXOptions.class);
-                    JavaFXModule.validateModules(javaFXOptions.getModules());
-
-                    var definedJavaFXModuleNames = new TreeSet<>(javaFXOptions.getModules());
-
-                    definedJavaFXModuleNames.forEach(javaFXModule -> moduleOptions.getAddModules().add(javaFXModule));
-                }
-            }
-        });
+        new ExecTask().configure(project);
     }
 }
