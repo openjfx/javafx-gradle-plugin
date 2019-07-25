b12a846c2f969e734cfc4a74edd1927f43cbcfd7
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2019-07-25 18:38:07 +0530

    Match beginning of the line while updating version

--- .ci/release.sh
+++ .ci/release.sh
@@ -9,7 +9,7 @@ newVersion=${TRAVIS_TAG%.*}.$((${TRAVIS_TAG##*.} + 1))
 # version 'TRAVIS_TAG' 
 # with 
 # version 'newVersion-SNAPSHOT'
-sed -i -z "0,/\nversion '$TRAVIS_TAG'/s//\nversion '$newVersion-SNAPSHOT'/" build.gradle
+sed -i "0,/^version '$TRAVIS_TAG'/s//version '$newVersion-SNAPSHOT'/" build.gradle
 
 git commit build.gradle -m "Upgrade version to $newVersion-SNAPSHOT" --author "Github Bot <githubbot@gluonhq.com>"
 git push https://gluon-bot:$GITHUB_PASSWORD@github.com/openjfx/javafx-gradle-plugin HEAD:master
